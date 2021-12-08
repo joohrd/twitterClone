c7ee8b694aff1e5b9a414ec475bc30940f1e7c24
Giovanni Rodrigues <giovannird01@gmail.com>
2021-12-08 19:16:46 -0300

    ...

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+
+	$erro = isset($_GET['erro']) ? $_GET['erro'] : 0;
+	//echo $erro;
+
+?>
+
+
 <!DOCTYPE HTML>
 <html lang="pt-br">
 	<head>
@@ -12,7 +20,30 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 	
 		<script>
-			// código javascript						
+			$(document).ready(function(){
+
+				$('#btn_login').click(function(){
+
+					var campo_vazio = false;
+
+					if($('#campo_usuario').val() == ''){
+						$('#campo_usuario').css({'border-color': '#A94442'});
+						campo_vazio = true;
+					}else{
+						$('#campo_usuario').css({'border-color': '#CCC'});
+					}
+
+					if($('#campo_senha').val() == ''){
+						$('#campo_senha').css({'border-color': '#A94442'});
+						campo_vazio = true;
+					}else{
+						$('#campo_senha').css({'border-color': '#CCC'});
+					}
+
+					if(campo_vazio) return false;
+
+				});
+			});			
 		</script>
 	</head>
 
@@ -34,7 +65,7 @@
 	        <div id="navbar" class="navbar-collapse collapse">
 	          <ul class="nav navbar-nav navbar-right">
 	            <li><a href="inscrevase.php">Inscrever-se</a></li>
-	            <li class="">
+	            <li class="<?= $erro == 1 ? 'open' : '' ?>">
 	            	<a id="entrar" data-target="#" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Entrar</a>
 					<ul class="dropdown-menu" aria-labelledby="entrar">
 						<div class="col-md-12">
@@ -54,6 +85,15 @@
 								<br /><br />
 								
 							</form>
+
+						<?php
+						
+							if($erro == 1 ){
+								echo '<font color="#FF0000">usuario e ou senha invalidos</font>';
+							}	
+						
+						?>
+
 						</form>
 				  	</ul>
 	            </li>
